f69fbb62f404c9c32955cd98a31aea600994f92b
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-09-22 15:17:45 +0900

    fix network to envoy and change socket_address address

--- upstream-app/run.sh
+++ upstream-app/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run -p 8080:8080 -d --name upstream-app upstream-app:latest
+name=upstream-app
+network="envoy"
+
+[[ `docker network ls | grep ${network} | wc -l` -ne 1 ]] && docker network create ${network}
+[[ `docker ps -a -f name=${name} -q | wc -l` -eq 1 ]] && docker stop ${name} && docker rm -v ${name}
+docker run --rm -d \
+  -p 8080:8080 \
+  --network ${network} \
+  --name ${name} ${name}:latest
