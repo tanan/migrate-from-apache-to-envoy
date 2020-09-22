1dd6a247e685e65a9261c4584e46a9fccc48106c
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-09-22 15:19:05 +0900

    fix port to 80

--- envoy/run.sh
+++ envoy/run.sh
@@ -5,7 +5,7 @@ network="envoy"
 
 [[ `docker network ls | grep ${network} | wc -l` -ne 1 ]] && docker network create ${network}
 [[ `docker ps -a -f name=${name} -q | wc -l` -eq 1 ]] && docker stop ${name} && docker rm -v ${name}
-docker run --rm -p 10000:10000 -d --name envoy \
+docker run --rm -p 80:80 -d --name envoy \
   -v $(pwd)/envoy.yaml:/etc/envoy/envoy.yaml \
   --network ${network} \
   envoyproxy/envoy-dev:5d95032baa803f853e9120048b56c8be3dab4b0d
