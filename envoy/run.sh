64569a4efbf7cea1495c0ab6aa9d6542b57afeab
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-09-24 21:59:54 +0900

    add cert

--- envoy/run.sh
+++ envoy/run.sh
@@ -5,8 +5,9 @@ network="envoy"
 
 [[ `docker network ls | grep ${network} | wc -l` -ne 1 ]] && docker network create ${network}
 [[ `docker ps -a -f name=${name} -q | wc -l` -eq 1 ]] && docker stop ${name} && docker rm -v ${name}
-docker run -p 80:80 -d --name envoy \
+docker run -p 80:80 -p 443:443 -d --name envoy \
   -v $(pwd)/envoy.yaml:/etc/envoy/envoy.yaml \
+  -v $(pwd)/certs:/etc/envoy/certs \
   -e "ENVOY_UID=0" \
   --network ${network} \
   envoyproxy/envoy-dev:5d95032baa803f853e9120048b56c8be3dab4b0d
